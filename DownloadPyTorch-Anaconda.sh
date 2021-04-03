de8787de0590467da9602d993425b993e74b5f18
minji <minji.jung@voithru.com>
2021-04-04 08:51:30 +0900

    fix: Change torch version

--- DownloadPyTorch-Anaconda.sh
+++ DownloadPyTorch-Anaconda.sh
@@ -2,4 +2,4 @@
 
 cuda=`echo $1 | sed -e "s/\.//g"`
 # /opt/miniconda3/bin/pip install http://download.pytorch.org/whl/cu${cuda}/torch-0.4.0-cp36-cp36m-linux_x86_64.whl
-/opt/anaconda3/bin/pip install http://download.pytorch.org/whl/cu91/torch-0.4.0-cp36-cp36m-linux_x86_64.whl
+/opt/anaconda3/bin/pip install http://download.pytorch.org/whl/cu91/torch-0.4.0-cp37-cp37m-linux_x86_64.whl
