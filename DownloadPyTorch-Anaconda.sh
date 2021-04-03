cbde040a552d911367f4327787e309b31894a6c8
minji <minji.jung@voithru.com>
2021-04-04 01:30:02 +0900

    fix: Specify cuda version

--- DownloadPyTorch-Anaconda.sh
+++ DownloadPyTorch-Anaconda.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cuda=`echo $1 | sed -e "s/\.//g"`
-/opt/miniconda3/bin/pip install http://download.pytorch.org/whl/cu${cuda}/torch-0.4.0-cp36-cp36m-linux_x86_64.whl
+# /opt/miniconda3/bin/pip install http://download.pytorch.org/whl/cu${cuda}/torch-0.4.0-cp36-cp36m-linux_x86_64.whl
+/opt/miniconda3/bin/pip install http://download.pytorch.org/whl/cu91/torch-0.4.0-cp36-cp36m-linux_x86_64.whl
